a32e232097e56d54898a1e2c728cfce8b677673c
Greg Leitkowski <greg_s_l@sbcglobal.net>
2018-06-02 22:14:35 -0400

    gold

--- boost.lua
+++ boost.lua
@@ -1,11 +1,21 @@
-local Boost = Powerup:new()
+local Boost = {}
+Boost.__index = Boost
 
-Boost.x = x 
-Boost.y = y
-Boost.radius = 10
-Boost.used = false
-Boost.color = {255, 215, 0}
-Boost.start_time = 0
+function Boost:new(x, y)
+    local b = {}
+    setmetatable(b, Boost)
+
+    b.x = x 
+    b.y = y
+    b.radius = 10
+    b.used = false
+    b.color = {255, 215, 0}
+    b.start_time = 0
+
+    return b
+end
+
+setmetatable(Boost, {__index = Powerup})
 
 function Boost:update(player1, player2)
     if self.used == false then
@@ -15,8 +25,12 @@ function Boost:update(player1, player2)
         if func.check_collision_circle(self, player2) then
             player2.boost_charges = player2.boost_charges + 1
         end
+        self.used = true
+        self.start_time = love.timer.getTime()
     else
-
+        if (love.timer.getTime() - self.start_time) >= 5 then
+            self.used = false
+        end
     end
 end
 
